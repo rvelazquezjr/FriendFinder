7066e6d62100c771a97022f6b4c37575413e2545
Enterprise D <51425632+rvelazquezjr@users.noreply.github.com>
2020-01-25 23:24:20 -0500

    302 me, please.

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+
+var port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+var apiRoutes = require('./app/routing/apiRoutes');
+var htmlRoutes = require('./app/routing/htmlRoutes');
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+app.use(function(err, req, res, next) {
+    console.error(err);
+    res.status(500).send('Something broke!');
+});
+
+
+
+app.listen(port, () => {
+    console.log(`App listening on ${port}`);
+});
